9906b44c84933f5c8b05ff5906ca426eba051d95
Marvin Wendt <github@marvinjwendt.com>
2019-08-06 01:49:42 +0200

    Add tests

--- test/typescript.test.ts
+++ test/typescript.test.ts
@@ -1,9 +1,2 @@
 import * as Pattern from '../src/index';
 
-test("abc", () => {
-  expect(Pattern.isTelephoneNumber("abc")).toBe(false)
-})
-
-test("(123) 456-7890", () => {
-  expect(Pattern.isTelephoneNumber("(123) 456-7890")).toBe(true)
-})
