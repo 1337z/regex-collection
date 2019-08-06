d7f7f57741a56c7359c036a156d7fcdcf6184653
Marvin Wendt <github@marvinjwendt.com>
2019-08-06 02:13:40 +0200

    feat: getTelephoneNumber

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-export const isTelephoneNumber = (text: string) => /[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}/i.test(text);
+export const telephoneNumber = /[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}/
+export const isTelephoneNumber = (text: string) => modify(telephoneNumber, "i").test(text);
+export const getTelephoneNumber = (text: string) => text.match(modify(telephoneNumber, "ig"))
+
+
+
+
+
+
+
+
+
+/**
+ * Adds flags to a regex
+ * @param regex A valid regex (Can be a variable from this module)
+ * @param flags The flags that will be added to the regex
+ */
+export function modify(regex: RegExp, flags: string) {
+  return new RegExp(telephoneNumber, flags)
+}
