936e6afac60efbf8c2ed2c60660b556fd58502e3
Marvin Wendt <github@marvinjwendt.com>
2019-08-06 18:26:22 +0200

    Add ipAddress

--- src/index.ts
+++ src/index.ts
@@ -52,7 +52,7 @@ export const getEmailAddress = (text: string) => get(text, emailAddress, "ig")
 /**
  * Matches IP addresses
  */
-export const ipAddress = /((?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?))*/
+export const ipAddress = /\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b/
 
 /**
  * Checks if *text* is an IP address
