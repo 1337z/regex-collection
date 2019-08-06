326baf8828d00ad2279fde248374306a2f0303af
Marvin Wendt <github@marvinjwendt.com>
2019-08-06 21:38:32 +0200

    Add email test with plus char

--- test/emailAddress.test.ts
+++ test/emailAddress.test.ts
@@ -1,7 +1,7 @@
 import * as search from "../src/index";
 import * as template from "./template"
 
-const valid = ["hello@world.com", "contact@example.com", "lastname.name@subdomain.example.com", "lastname.name@example.com", "googlemail@gmail.com"]
+const valid = ["hello@world.com", "contact@example.com", "lastname.name@subdomain.example.com", "lastname.name@example.com", "googlemail@gmail.com", "e+mail@domain.com"]
 const invalid = ["hello", "Hello@world", "hello.com", "hello@world."]
 
 template.run(search.isEmailAddress, search.getEmailAddress, valid, invalid)
