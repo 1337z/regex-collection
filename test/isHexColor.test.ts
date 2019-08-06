47007d5280b1aa8da321ef434cbba77e4d32d654
Marvin Wendt <github@marvinjwendt.com>
2019-08-06 04:26:46 +0200

    Add more tests

--- test/isHexColor.test.ts
+++ test/isHexColor.test.ts
@@ -16,6 +16,14 @@ test("#ffffff", () => {
   expect(search.isHexColor("#ffffff")).toBe(true)
 })
 
+test("#352791", () => {
+  expect(search.isHexColor("#352791")).toBe(true)
+})
+
+test("#464f4a", () => {
+  expect(search.isHexColor("#464f4a")).toBe(true)
+})
+
 // Hex color codes in text
 
 test("#fff", () => {
