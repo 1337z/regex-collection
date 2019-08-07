b4166b50ead501008629ee630e526816ac7773c0
Marvin Wendt <github@marvinjwendt.com>
2019-08-07 15:11:01 +0200

    Add framework tests

--- test/mainFramework.test.ts
+++ test/mainFramework.test.ts
@@ -1,6 +1,10 @@
 import * as search from "../src/index";
 import * as template from "./template"
 
-test("modify", () => {
+test("modify g flag", () => {
   expect(search.modify(/a/, "g")).toStrictEqual(/a/g)
+})
+
+test("modify i flag", () => {
+  expect(search.modify(/a/, "i")).toStrictEqual(/a/i)
 })
